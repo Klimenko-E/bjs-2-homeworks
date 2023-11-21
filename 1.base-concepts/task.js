9d552f750cd9cf9756bbeb2721b49eb65a9287c8
Klimenko.E <klimenko.e@bk.ru>
2023-11-21 11:48:31 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+ "use strict"
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  function solveEquation(a, b, c) {
+  	let arr = [];
+  	let d = Math.pow(b, 2) - 4 * a * c;
+
+  	if (d < 0) {
+  		arr = [];
+  		return arr;
+		
+  	} else if (d === 0) {
+  		arr = [-b / (2 * a)];
+  		return arr;
+
+  	} else {
+  		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+
+  		return arr;
+
+  	}
+  }
+
+  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  	let monthlyPercent = percent / 12 / 100;
+  	let loanBody = amount - contribution;
+  	let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+  	let totalAmount = +(monthlyPayment * countMonths).toFixed(2);
+  	
+	return totalAmount;
+  }
