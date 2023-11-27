7a81adec174865f6a238b3e8d3e7a8481cc79d2b
Klimenko.E <klimenko.e@bk.ru>
2023-11-27 13:57:40 +0300

    Третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, array) => {
+		acc += age;
+		if (index === array.length - 1) {
+			return acc / array.length
+		}
+		return acc;
+
+	}, 0);
+
 }
